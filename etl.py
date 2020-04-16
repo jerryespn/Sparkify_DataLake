f5b471612d07f39036eb671da8cd41ca6ab37246
jerryespn <gerardoesp.lira@gmail.com>
2020-04-15 22:58:52 -0500

    Initial Commit. Script and Jupyter Notebook Creation

---
+++ etl.py
@@ -0,0 +1,117 @@
+# Sparkify - Data Engineer Nanodegree program SQL Data Model Project
+# By JGEL
+# April 2020
+
+import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+
+
+# Reading configuration file
+config = configparser.ConfigParser()
+config.read('dl.cfg')
+
+# AWS access keys taken from configuration file
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+
+# SparkContext Initialize
+def create_spark_session():
+    '''
+    Initialize the SparkSession for the application, includes AWS package to use it
+    '''
+    spark = SparkSession \
+        .builder \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+        .getOrCreate()
+    return spark
+
+# Process Song Function
+def process_song_data(spark, input_data, output_data):
+    '''
+    This function extracts and process the song and artist data from S3 into DataFrames,
+    then transforms, and loads back into S3.
+    :input_data -> S3 Bucket where data comes
+    :output_data -> S3 Bucket where data is stored
+    '''
+    # get filepath to song data file
+    song_data = input_data + "song_data/*/*/*"
+    
+    # read song data file
+    df = spark.read.json(song_data)
+    
+    # extract columns to create songs table
+    songs_table = 
+    
+    # write songs table to parquet files partitioned by year and artist
+    songs_table
+
+    # extract columns to create artists table
+    artists_table = 
+    
+    # write artists table to parquet files
+    artists_table
+
+
+def process_log_data(spark, input_data, output_data):
+    '''
+    This function extracts and process the log data from S3 into DataFrames,
+    :input_data -> S3 Bucket where data comes
+    :output_data -> S3 Bucket where data is stored
+    '''
+    # get filepath to log data file
+    log_data =
+
+    # read log data file
+    df = 
+    
+    # filter by actions for song plays
+    df = 
+
+    # extract columns for users table    
+    artists_table = 
+    
+    # write users table to parquet files
+    artists_table
+
+    # create timestamp column from original timestamp column
+    get_timestamp = udf()
+    df = 
+    
+    # create datetime column from original timestamp column
+    get_datetime = udf()
+    df = 
+    
+    # extract columns to create time table
+    time_table = 
+    
+    # write time table to parquet files partitioned by year and month
+    time_table
+
+    # read in song data to use for songplays table
+    song_df = 
+
+    # extract columns from joined song and log datasets to create songplays table 
+    songplays_table = 
+
+    # write songplays table to parquet files partitioned by year and month
+    songplays_table
+
+
+def main():
+    """
+    Main Function where the etl process is executed, every function is called from here
+    """
+    spark = create_spark_session()
+    input_data = "s3a://udacity-dend/"
+    output_data = ""
+    
+    process_song_data(spark, input_data, output_data)    
+    process_log_data(spark, input_data, output_data)
+
+
+if __name__ == "__main__":
+    main()
